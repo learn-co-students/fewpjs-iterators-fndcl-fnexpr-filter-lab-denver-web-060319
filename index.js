5716ddda95002d1da68a89d6720e816492a0abb4
little-Dave <17536424+little-Dave@users.noreply.github.com>
2019-08-13 08:13:08 -0600

    Practice using '.filter()'

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Code your solution here
+
+
+function findMatching(array, string) {
+  return array.filter(driver => {return driver.toLowerCase() === string.toLowerCase()})
+}
+
+function fuzzyMatch(array, string) {
+  return array.filter(driver => {return driver.toLowerCase().slice(0, string.length) === string.toLowerCase()})
+}
+
+function matchName(array, string) {
+  return array.filter(driver => {return driver.name.toLowerCase() === string.toLowerCase()})
+}
